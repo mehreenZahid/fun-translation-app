40e1908fcfd6aeb0bc2ebbf18f0e59f1874e0a53
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-12-10 01:51:20 +0530

    updates

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txtarea-input");
+var divOutput = document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/groot.json"
+
+function getTranslationURL(input){
+    return serverURL +"?"+ "text="+ input
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something went wrong with server! Try again after some time")
+}
+
+function clickEventHandler(){
+    var inputText = txtInput.value
+
+    fetch(getTranslationURL(inputText))
+     .then(response => response.json())
+     .then(json => {
+        var translatedText = json.contents.translated
+        divOutput.innerText = translatedText
+     })
+     .catch(errorHandler)
+    
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
